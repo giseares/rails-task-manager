43cc0264d6c54bae19511e3c25b7e150a7df5a9c
Gise Ares <gise.ares@gmail.com>
2020-07-21 17:13:58 -0300

    falta el destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,49 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    if @task.completed
+      @response = "Task is done"
+    else 
+      @response = "Do your task!!"
+    end
+  end
+
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit 
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params_update)
+    redirect_to tasks_path
+  end
+
+  def delete
+    @task = Task.find(params:[id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def task_params_update
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
